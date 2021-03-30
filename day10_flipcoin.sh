cd7fde8d69608e7b05c55a72c47b3313be5ba057
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-03-31 00:55:08 +0530

    Added: % calculation

--- day10_flipcoin.sh
+++ day10_flipcoin.sh
@@ -2,7 +2,46 @@
 x=$((RANDOM%2))
 if [ $x -eq 0 ]
 then
-echo "head wins"
+	echo "head wins"
 else
-echo "tail wins"
+	echo "tail wins"
 fi
+
+#!/bin/bash 
+header=0;
+tailer=0;
+a=0;
+b=0;
+counter=0
+trolly=0
+
+declare -A e
+for (( i=1; i<10; i++ ))
+do
+	x=$((RANDOM%2))
+	if [ $x -eq 0 ]
+	then
+		echo "head wins"
+		let "header++"
+		declare -A d
+
+		d[$counter]="head"
+		((counter++))
+
+	else
+
+
+		echo "tail wins"
+		let "tailer++"
+		e[$trolly]="tail"
+		((trolly++))
+	fi
+done
+echo  "header: $header"
+echo  "tailer: $tailer"
+echo "${d[@]}"
+echo "${e[@]}"
+
+total=$(( $header + $tailer ))
+echo "(($header / $total))"
+echo "tailor wins percentage : (($tailer / $total))"
